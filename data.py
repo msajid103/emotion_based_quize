d424c5a80f66635fe8bd34062ad25b660df4c960
msajid103 <sajid2021@namal.edu.pk>
2024-07-04 21:24:48 -0700

    second commit

---
+++ data.py
@@ -0,0 +1,52 @@
+
+quiz_data = [
+     {
+        'heading': "General Knowledge Quiz",
+        
+    },
+    {
+        'question': "What is the capital of France?",
+        'options': [
+            {'id': 'a', 'text': 'Berlin'},
+            {'id': 'b', 'text': 'Madrid'},
+            {'id': 'c', 'text': 'Paris'},
+            {'id': 'd', 'text': 'Rome'}
+        ]
+    },
+    {
+        'question': "Which planet is known as the Red Planet?",
+        'options': [
+            {'id': 'a', 'text': 'Earth'},
+            {'id': 'b', 'text': 'Mars'},
+            {'id': 'c', 'text': 'Jupiter'},
+            {'id': 'd', 'text': 'Saturn'}
+        ]
+    },
+    {
+        'question': "Who wrote 'Hamlet'?",
+        'options': [
+            {'id': 'a', 'text': 'Charles Dickens'},
+            {'id': 'b', 'text': 'Jane Austen'},
+            {'id': 'c', 'text': 'William Shakespeare'},
+            {'id': 'd', 'text': 'Mark Twain'}
+        ]
+    },
+    {
+        'question': "What is the largest ocean on Earth?",
+        'options': [
+            {'id': 'a', 'text': 'Atlantic Ocean'},
+            {'id': 'b', 'text': 'Indian Ocean'},
+            {'id': 'c', 'text': 'Arctic Ocean'},
+            {'id': 'd', 'text': 'Pacific Ocean'}
+        ]
+    },
+    {
+        'question': "What is the square root of 64?",
+        'options': [
+            {'id': 'a', 'text': '6'},
+            {'id': 'b', 'text': '7'},
+            {'id': 'c', 'text': '8'},
+            {'id': 'd', 'text': '9'}
+        ]
+    }
+]
